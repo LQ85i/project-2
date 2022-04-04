d42d67bc9c5941e2d099e57c4bf5e75e5ff28281
LQ85i <39389364+LQ85i@users.noreply.github.com>
2022-04-04 21:19:58 +0300

    Add first version of game

--- scripts.js
+++ scripts.js
@@ -29,39 +29,79 @@ function playRound(playerSelection, computerSelection) {
     || (playerSelection == "ROCK" && computerSelection == "ROCK")){
         return "It's a tie! You both played the same";
     }
-    
 }
 
-function game(){
-    let rounds = 5;
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i<rounds; i++){
-        let playerSelection = window.prompt("Write your choice here. [ROCK / PAPER / SCISSORS]","");
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection,computerSelection);
-        console.log(result);
-        if (result == "You Win! Paper beats Rock"
-        || result == "You Win! Rock beats Scissors"
-        || result == "You Win! Scissors beats Paper") {
-            playerScore++;
-        } 
-        else if (result == "You Lose! Scissors beats Paper"
-        || result == "You Lose! Paper beats Rock"
-        || result == "You Lose! Rock beats Scissors") {
-            computerScore++;
-        }
+function game(button){
+    const div = document.createElement('div');
+    div.className = 'result';
+    document.querySelector('.results').prepend(div);
+    const result = playRound(button.id, computerPlay());
+    div.textContent = result;
+    div.style.backgroundColor = '#ededed';
+    div.style.width = '300px';
+    div.style.textAlign = 'center';
+    div.style.borderRadius = '3px';
+    const playerScore = document.querySelector('.player .score');
+    const computerScore = document.querySelector('.computer .score');
+    const player = document.querySelector('.player');
+    const computer = document.querySelector('.computer');
+    if(result.includes("Win")){
+        div.style.backgroundColor = '#70db56';
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
     }
-    if(playerScore > computerScore) {
-        console.log("You Win in total points! You had " + playerScore + " points and Computer had " + computerScore + " points.");
+    else if(result.includes("Lose")){
+        div.style.backgroundColor = '#d6515c';
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
     }
-    else if(playerScore < computerScore) {
-        console.log("You Lose in total points! You had " + playerScore + " points and Computer had " + computerScore + " points.");
+    const playerScoreInt = parseInt(playerScore.textContent);
+    const computerScoreInt = parseInt(computerScore.textContent);
+    if(playerScoreInt > computerScoreInt){
+        player.style.backgroundColor = '#70db56';
+        computer.style.backgroundColor = '#ededed';
     }
-    else if(playerScore == computerScore) {
-        console.log("You are Tied in total points! You had " + playerScore + " points and Computer had " + computerScore + " points.");
+    else if(playerScoreInt < computerScoreInt){
+        player.style.backgroundColor = '#ededed';
+        computer.style.backgroundColor = '#d6515c';
     }
-    
+    else {
+        player.style.backgroundColor = '#ededed';
+        computer.style.backgroundColor = '#ededed';
+    }
+    if(playerScoreInt == 5){
+        document.querySelector('.counter .title').textContent = 'Player Wins!';
+        const playButtons = document.querySelector('.buttons .left');
+        playButtons.className = 'left-inactive';
+    }
+    else if(computerScoreInt == 5){
+        document.querySelector('.counter .title').textContent = 'Computer Wins!';
+        const playButtons = document.querySelector('.buttons .left');
+        playButtons.className = 'left-inactive';
+    }
 }
 
-game();
+function reset(){
+    const results = document.querySelectorAll('.results .result');
+    results.forEach(e => e.remove());
+    const player = document.querySelector('.player');
+    const computer = document.querySelector('.computer');
+    const playerScore = document.querySelector('.player .score');
+    const computerScore = document.querySelector('.computer .score');
+    playerScore.textContent = 0
+    computerScore.textContent = 0
+    player.style.backgroundColor = '#ededed';
+    computer.style.backgroundColor = '#ededed';
+    document.querySelector('.counter .title').textContent = 'Total Points:';
+
+    const playButtons = document.querySelectorAll('.buttons .left-inactive');
+    playButtons.forEach(e => e.className = 'left');
+}
+
+const playButtons = document.querySelectorAll('.play');
+
+playButtons.forEach((button) => {
+    button.addEventListener('click', () => game(button));
+});
+
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', () => reset());
+
